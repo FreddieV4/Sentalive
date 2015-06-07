3614fbe664f150b1766d3d0d09196c1ab1904933
unknown <cjm5772@rit.edu>
2015-06-06 23:11:39 -0500

    Functions and stuff

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ app.listen(appEnv.port, appEnv.bind, function() {
 // print a message when the server starts listening
 console.log("server starting on " + appEnv.url);
 
+)}
+
   ///////////////
   // Variables //
   ///////////////
@@ -50,89 +52,102 @@ console.log("server starting on " + appEnv.url);
       negative:0,
       neutral:0,
       ambivalent:0,
-    };
+    }
+  };
   var http = require("https");
-  var input = 'ah8';
+  var input = '#angelhack';
   var options = 'https://821f292fdc3ca76b1a542b7edfd52ea9:AhzRt1NRAW@cdeservice.mybluemix.net:443/api/v1/messages/search?q=' + input;
   var eventDate = new Date("2015-06-06T09:00:00.000Z");
+  var eventEndDate = new Date("2015-06-07T18:00:00.000Z");
 
   //JSON object filled with an array of "tweets", each with their own data
-  var request = http.get(options, function(res){
+  var request = http.get(options, function(response){
     // Teddy is our hero
-    res.on('data', function(chunk){
-      console.log(chunk.toString());
-    })
-    // Temp vars
-    var sentiment;
-    var tweetDate;
-    // Foreach loop to iterate through the JSON tweets
-    //for(var tweet in res["tweets"])
-    for(var i=0; i<res.tweets.length; i++)
+    response.on('data', function(chunk)
     {
-      // Store the date as a date type, instead of a string
-      tweetDate = new Date(res.tweets[i].message.postedTime);
-      // Check to see if the tweet is within range
-      if( checkWithinMaxRange(tweetDate, eventDate) ) {
-        // Store the sentiment so I don't have to write it out every time
-        sentiment = res.tweets[i].content_sentiment.polarity;
-        // Do a switch on the time comparison
-        switch ( compareTime(tweetDate, eventDate) )
+      // Convert the JSON string into a javascript JSON object
+      var res = JSON.parse(chunk.toString());
+      // Temp vars
+      var sentiment;
+      var tweetDate;
+      // Foreach loop to iterate through the JSON tweets
+      for(var i=0; i<res.tweets.length; i++)
+      {
+        // Store the date as a date type, instead of a string
+        tweetDate = new Date(res.tweets[i].message.postedTime);
+        // Check to see if the tweet is within range
+        if( isWithinRange(tweetDate, eventDate) )
         {
-          // If tweet is before the event
-          case -1:
-              // Add text so Watson can use it
-              tweetData.before.text+=res.tweets[i].message.body;
-              // Compare sentiment and add vars accordingly
-              if(sentiment == "positive")
-                tweetData.before.positive++;
-              else if(sentiment == "negative")
-                tweetData.before.negative++;
-              else if(sentiment == "neutral")
-                tweetData.before.neutral++;
-              else
-                tweetData.before.ambivalent++;
-              break;
-          // If tweet is during the event
-          case 0:
-              tweetData.during.text+=res.tweets[i].message.body;
-              if(sentiment == "positive")
-                tweetData.during.positive++;
-              else if(sentiment == "negative")
-                tweetData.during.negative++;
-              else if(sentiment == "neutral")
-                tweetData.during.neutral++;
-              else
-                tweetData.during.ambivalent++;
-              break;
-          // If tweet is after the event
-          case 1:
-              tweetData.after.text+=res.tweets[i].message.body;
-              if(sentiment == "positive")
-                tweetData.after.positive++;
-              else if(sentiment == "negative")
-                tweetData.after.negative++;
-              else if(sentiment == "neutral")
-                tweetData.after.neutral++;
-              else
-                tweetData.after.ambivalent++;
-              break;
+          // Store the sentiment so I don't have to write it out every time
+          sentiment = res.tweets[i].content_sentiment.polarity;
+          // Do a switch on the time comparison
+          switch ( sortTweets(tweetDate, eventDate, eventEndDate) )
+          {
+            // If tweet is before the event
+            case -1:
+                // Add text so Watson can use it
+                tweetData.before.text+=res.tweets[i].message.body;
+                // Compare sentiment and add vars accordingly
+                if(sentiment == "positive")
+                  tweetData.before.positive++;
+                else if(sentiment == "negative")
+                  tweetData.before.negative++;
+                else if(sentiment == "neutral")
+                  tweetData.before.neutral++;
+                else
+                  tweetData.before.ambivalent++;
+                break;
+            // If tweet is during the event
+            case 0:
+                tweetData.during.text+=res.tweets[i].message.body;
+                if(sentiment == "positive")
+                  tweetData.during.positive++;
+                else if(sentiment == "negative")
+                  tweetData.during.negative++;
+                else if(sentiment == "neutral")
+                  tweetData.during.neutral++;
+                else
+                  tweetData.during.ambivalent++;
+                break;
+            // If tweet is after the event
+            case 1:
+                tweetData.after.text+=res.tweets[i].message.body;
+                if(sentiment == "positive")
+                  tweetData.after.positive++;
+                else if(sentiment == "negative")
+                  tweetData.after.negative++;
+                else if(sentiment == "neutral")
+                  tweetData.after.neutral++;
+                else
+                  tweetData.after.ambivalent++;
+                break;
+            default:
+                console.log("Something went wrong in the switch statement");
+          }
         }
       }
-    }
+    });
   });
 
 ///////////////
 // Functions //
 ///////////////
 //Returns whether or not the tweet time is before or after the event
-function compareTime(tweetTime, eventTime)
-{
-  // Note that time is a string
-  var tweetDate = new Date(tweetTime);
-  //if()
+function sortTweets(tweetTime, eventStart, eventEnd){
+  if (tweetTime.getTime() < eventStart.getTime()){
+    return -1;
+  } else if (tweetTime.getTime() < eventEnd.getTime()){
+    return 0;
+  } else {
+    return 1;
+  }
 }
 // Returns true if the tweet is within 30 days (subject to change) of the event
-function checkWithinMaxRange(tweetTime, eventTime)
-{
-  if(tweetTime.Days)
+function isWithinRange(tweetTime, eventStart, eventEnd) {
+  var numDays = 30;
+  var startTime = eventStart.getTime() - (1000*60*60*24*numDays);
+  if (tweetTime.getTime() > startTime) {
+    return true;
+  }
+  return false;
 }
